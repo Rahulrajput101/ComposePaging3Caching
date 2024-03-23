141ca685793fee80045df6c8164d39d65cfb892b
Rahulrajput101 <itsmerahul10534952@gmail.com>
2024-03-23 23:58:50 +0530

    Data Layers setup

---
+++ app/src/main/java/com/plcoding/composepaging3caching/data/remote/BeerRemoteMediator.kt
@@ -0,0 +1,23 @@
+package com.plcoding.composepaging3caching.data.remote
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
+import androidx.paging.RemoteMediator
+import com.plcoding.composepaging3caching.data.local.entity.BeerDatabase
+import com.plcoding.composepaging3caching.data.local.entity.BeerEntity
+@OptIn(ExperimentalPagingApi::class)
+class BeerRemoteMediator(
+    private val beerDb : BeerDatabase,
+    private val beerApi: BeerApi
+) : RemoteMediator<Int, BeerEntity>(){
+
+    override suspend fun load(
+        loadType: LoadType,
+        state: PagingState<Int, BeerEntity>
+    ): MediatorResult {
+        TODO("Not yet implemented")
+    }
+
+
+}
---
+++ app/src/main/java/com/plcoding/composepaging3caching/doamin/model/Beer.kt
@@ -0,0 +1,10 @@
+package com.plcoding.composepaging3caching.doamin.model
+
+data class Beer(
+    val id: Int,
+    val name: String,
+    val tagline: String,
+    val firstBrewed: String,
+    val description: String,
+    val imageUrl: String?
+)
